7842c1031ce698f83b1dfcd7e5d63bff61244225
Akoly Vongdala <akolyv@uw.edu>
2021-04-27 16:28:19 -0700

    Rename data file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
+import {csv} from "d3-fetch";
 
 const viewHeight = 500;
 const viewWidth = 500;
 
 const App = () => {
+    csv("https://raw.githubusercontent.com/AkolyVongdala/A2-Exploratory-Data-Analysis/main/data/weather%20(1).csv"
+    ).then((data) => console.log(data));
     return (
-        <svg style={{ border: "5px solid red", width:viewWidth, height:viewHeight}}>
-            <circle cx={20} cy={20} r="5"></circle>
-            <rect x={200} y={200} width={10} height={10} fill="pink"/>
-            <rect x={212} y={200} width={10} height={10} fill="black"/>
-            <rect x={224} y={200} width={10} height={10} fill="black"/>
-            <line x1={0} y1={viewHeight} x2={150} y2={100} stroke="orange"/>
-            <text x="50" y="50" style={{ font: "italic 16px" }}>
-                ummmmm add something lol
-            </text>
-            <text> adding another thing</text>
-        </svg>
+        <div>
+            <p>Data!</p>
+        </div>
     );
     
 };
